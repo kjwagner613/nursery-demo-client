eac16b325575456e53eb90206d075fabe0744dca
kjwagner613 <kjwagner613@gmail.com>
2025-06-08 18:53:34 -0700

    adding global background to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#F3F4F6", // Light gray background
+      },
+    },
+
   },
   plugins: [],
 };
